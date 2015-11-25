a5f78bedebe7e304ca82c2c779eacca6d1be117b
Dominik Winter <dominik.winter@sydev.de>
2015-11-25 20:51:24 +0100

    Added background-color classes in less and css

--- js/dropdown/dropdown.js
+++ js/dropdown/dropdown.js
@@ -2,63 +2,64 @@
 
 angular.module('dropdown', [])
 	.directive('dropdown', ['$window', '$document', function($window, $document) {
-    return {
-      restrict: 'A',
-      link: function(scope, ele) {
-        var eleRect = ele[0].getBoundingClientRect();
+		return {
+			restrict: 'A',
+			link: function(scope, ele) {
+				var eleRect = ele[0].getBoundingClientRect();
 
-        ele.parent().css({
-          position: 'relative',
-          'z-index': 100
-        });
+				ele.parent().css({
+					position: 'relative',
+					'z-index': 100
+				});
 
-        ele.addClass('dropdown');
-        ele.children()[0].classList.add('dropdown-toggle');
-        ele.children()[1].classList.add('dropdown-menu');
+				ele.addClass('dropdown');
+				ele.children()[0].classList.add('dropdown-toggle');
+				ele.children()[1].classList.add('dropdown-menu');
 
-        if ($window.innerWidth < eleRect.left + 160) {
-          angular.element(ele[0].getElementsByClassName('dropdown-menu')).addClass('toLeft');
-        }
+				if ($window.innerWidth < eleRect.left + 160) {
+					angular.element(ele[0].getElementsByClassName('dropdown-menu')).addClass(
+						'toLeft');
+				}
 
-        function hide(event) {
-          var target = event.target,
-            dropdowns = document.querySelectorAll('[dropdown]'),
-            check, checkClass;
+				function hide(event) {
+					var target = event.target,
+						dropdowns = document.querySelectorAll('[dropdown]'),
+						check, checkClass;
 
-          while (target) {
-            check       = target.getAttribute('dropdown');
-            checkClass  = target.classList.contains('dropdown-menu');
+					while (target) {
+						check = target.getAttribute('dropdown');
+						checkClass = target.classList.contains('dropdown-menu');
 
-            if (check !== null) {
-              for (var i = 0, len = dropdowns.length; i < len; i++) {
-                dropdowns[i].classList.remove('open');
-              }
-              target.classList.add('open');
-              return;
-            } 
+						if (check !== null) {
+							for (var i = 0, len = dropdowns.length; i < len; i++) {
+								dropdowns[i].classList.remove('open');
+							}
+							target.classList.add('open');
+							return;
+						}
 
-            if (checkClass) {
-              for (var i = 0, len = dropdowns.length; i < len; i++) {
-                dropdowns[i].classList.remove('open');
-              }
-              return;
-            }
+						if (checkClass) {
+							for (var i = 0, len = dropdowns.length; i < len; i++) {
+								dropdowns[i].classList.remove('open');
+							}
+							return;
+						}
 
-            target = target.parentElement;
-          }
+						target = target.parentElement;
+					}
 
-          for (var i = 0, len = dropdowns.length; i < len; i++) {
-            dropdowns[i].classList.remove('open');
-          }
-        }
+					for (var i = 0, len = dropdowns.length; i < len; i++) {
+						dropdowns[i].classList.remove('open');
+					}
+				}
 
-        ele.on('click', function() {
-          ele.toggleClass('open');
-        });
+				ele.on('click', function() {
+					ele.toggleClass('open');
+				});
 
-        $document.on('click', function(e) {
-          hide(e);
-        });
-      }
-    };
-  }]);
+				$document.on('click', function(e) {
+					hide(e);
+				});
+			}
+		};
+	}]);
